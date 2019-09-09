2bc1a629c272299a1a4f3b486144e6d99adb8019
adoe21 <doe@exanode-3-7.local>
2019-09-09 09:48:29 -0700

    tidying up

--- .snakemake/conda/a9dff4f3/lib/python3.7/site-packages/dnaio/_util.py
+++
@@ -1,7 +0,0 @@
-def shorten(s, n=100):
-    """Shorten string s to at most n characters, appending "..." if necessary."""
-    if s is None:
-        return None
-    if len(s) > n:
-        s = s[:n-3] + '...'
-    return s
