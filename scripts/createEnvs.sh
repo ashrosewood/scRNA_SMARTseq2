e0abda2ade045258d029b35016410fe8dd13d686
ashrosewood <ashleyrosewoodfin@gmail.com>
2020-03-16 12:05:55 -0700

    rebuilt envs with conda env export --no-builds

---
+++ scripts/createEnvs.sh
@@ -0,0 +1,47 @@
+## must recreate all envs
+## you will want to remove all packages and envs after export to prevent your home directory from hitting 10G and locking you out
+
+#################
+## rules/align_rmdp.smk 
+#################
+conda env create -f envs/trimG.yaml
+conda activate trimG.yaml
+conda env export --no-builds > envs/trimG.yaml
+conda deactivate
+conda remove --name trimG.yaml --all
+
+## not working make from scratch
+conda create -f envs/Fastqscreen.yaml --name screen
+
+conda create --name Fastqscreen -c bioconda fastq-screen
+conda activate Screen
+conda env export --no-builds > envs/fastqscreen.yaml
+conda deactivate
+conda remove Screen
+
+conda env create -f featureCounts.yaml
+conda activate Fcounts
+conda env export --no-builds > envs/featureCounts.yaml
+conda deactivate 
+conda remove --name Fcounts --all
+
+#############
+## rules/analysis.smk
+#############
+conda create -n smartseq2 r-seurat bioconductor-singlecellexperiment r-data.table bioconductor-rsubread r-purrr bioconductor-rtracklayer r-ggplot2 r-cowplot bioconductor-scater r-png
+conda activte smartseq2
+conda env export --no-builds > envs/scAnalysis.yaml
+conda deactivate
+conda remove --name smartseq2 --all
+
+
+conda create env envs/NMT_Create_Seurat.yaml
+## failed
+conda activate NMT_Create_Seurat
+conda env export --no-builds > envs/NMT_Create_Seurat.new.yaml
+conda deactivate
+conda remove --name NMT_create_Seurat --all
+
+grep prefix envs/* | sed "s/\:.*//g" | while read line; do
+    sed -i "s/prefix.*//g"
+done
